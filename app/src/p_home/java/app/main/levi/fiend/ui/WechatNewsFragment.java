6cc1d53e8a14cb373ce505420b9af23716906e06
LeviWGG <yugiop@126.com>
2018-05-16 23:05:38 +0800

    update 05/16

--- app/src/p_home/java/app/main/levi/fiend/ui/WechatNewsFragment.java
+++ app/src/p_home/java/app/main/levi/fiend/ui/WechatNewsFragment.java
@@ -3,14 +3,18 @@ package app.main.levi.fiend.ui;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import app.main.levi.fiend.R;
 import app.main.levi.fiend.bean.Weixin;
 import app.main.levi.fiend.contract.IWechatContract;
 import app.main.levi.fiend.contract.WechatPresenterImp;
+import app.main.levi.fiend.ui.adapter.WechatNewsAdapter;
 import app.main.wangliwei.baselib.base.BaseMVPFragment;
 import app.main.wangliwei.baselib.base.BasePresenter;
+import butterknife.BindView;
 
 /**
  * Created by wlw on 2018/5/9.
@@ -18,6 +22,11 @@ import app.main.wangliwei.baselib.base.BasePresenter;
 
 public class WechatNewsFragment extends BaseMVPFragment<IWechatContract.IWechatPresenter>
         implements IWechatContract.IWechatView {
+
+    @BindView(R.id.recycler_only)
+    RecyclerView recyclerView;
+
+    private WechatNewsAdapter adapter;
 
     @Override
     public int getLayoutId() {
@@ -32,7 +41,9 @@ public class WechatNewsFragment extends BaseMVPFragment<IWechatContract.IWechatP
 
     @Override
     public void initView(@Nullable Bundle savedInstanceState) {
+        adapter = new WechatNewsAdapter();
 
+        recyclerView.setLayoutManager(new LinearLayoutManager(_mActivity));
     }
 
     @Override
---
+++ app/src/p_home/java/app/main/levi/fiend/ui/adapter/WechatNewsAdapter.java
@@ -0,0 +1,32 @@
+package app.main.levi.fiend.ui.adapter;
+
+import android.support.annotation.Nullable;
+import android.widget.ImageView;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+
+import java.util.List;
+
+import app.main.levi.fiend.R;
+import app.main.levi.fiend.bean.Weixin;
+import app.main.wangliwei.baselib.base.BaseMVPFragment;
+import app.main.wangliwei.baselib.utils.GlideApp;
+
+/**
+ * Created by wangliwei on 2018/5/16.
+ */
+
+public class WechatNewsAdapter extends BaseQuickAdapter<Weixin.ResultBean.ListBean,BaseViewHolder> {
+
+    public WechatNewsAdapter() {
+        super(R.layout.item_wechat_news, null);
+    }
+
+    @Override
+    protected void convert(BaseViewHolder helper, Weixin.ResultBean.ListBean item) {
+        GlideApp.with(mContext).load(item.getFirstImg()).into((ImageView) helper.getView(R.id.view_picture));
+        helper.setText(R.id.text_title,item.getTitle())
+                .setText(R.id.text_source,item.getSource());
+    }
+}
